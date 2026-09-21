d5832e7e042108b5870cdf70836a7008ca9ef2a4
prakash-lost <payopayo050@gmail.com>
2026-09-21 11:16:49 +0545

    Added more teams

--- src/pages/TeamsPage.jsx
+++ src/pages/TeamsPage.jsx
@@ -3,10 +3,10 @@ import React, { useEffect, useState } from "react";
 
 const TeamsPage = () => {
   const [putTeams, setPutTeams] = useState([]);
-  const[visible,setVisible]=useState(12)
-  const HandleVisible=()=>{
-    setVisible((prev)=>prev+4)
-  }
+  const [visible, setVisible] = useState(12);
+  const HandleVisible = () => {
+    setVisible((prev) => prev + 4);
+  };
   useEffect(() => {
     const getTeams = async () => {
       try {
@@ -25,25 +25,32 @@ const TeamsPage = () => {
   //   console.log(`received`,putTeams)
   // },[putTeams])
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 gap-4 hover:cursor-pointer">
-      {putTeams.slice(0,visible).map((member) => (
-        <div
-          key={member.id}
-          className="h-32 overflow-hidden rounded-lg  text-white mt-2 hover:scale-[1.05]"
-        >
-          <img
-            src={member.name}
-            alt={member.shortName}
-            className="object-cover h-full w-full bg-slate-900"
-          />
-        </div>
-      ))}
-      {visible< putTeams.length &&
-      <div className="flex justify-center items-center ">
-        <button onClick={HandleVisible}>Load More</button>
+    <>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 hover:cursor-pointer">
+        {putTeams.slice(0, visible).map((member) => (
+          <div
+            key={member.id}
+            className="h-32 overflow-hidden rounded-lg  text-white mt-2 hover:scale-[1.05]"
+          >
+            <img
+              src={member.name}
+              alt={member.shortName}
+              className="object-cover h-full w-full bg-slate-900"
+            />
+          </div>
+        ))}
       </div>
-      }
-    </div>
+      {visible < putTeams.length && (
+        <div className="w-full flex justify-center items-center my-6">
+          <button
+            onClick={HandleVisible}
+            className="bg-orange-500 text-white rounded-lg px-4 py-2 text-center font-medium  hover:bg-orange-600 hover:scale-110 active:scale-70" 
+          >
+            Load More
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
